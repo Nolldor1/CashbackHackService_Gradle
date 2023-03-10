2c4ac5b0b83e73852eb89c55a2e0129e39a053a2
German Gizatullin <snolldors@inbox.ru>
2023-03-11 00:09:55 +0500

    add new tests for Junit 5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,9 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
     @Test
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
 
         int actual = 1;
         int expected = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     public void testLowerBorderValue() {
@@ -22,7 +22,7 @@ public class CashbackHackServiceTest {
 
         int actual = 999;
         int expected = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     public void testZeroValue() {
@@ -31,7 +31,7 @@ public class CashbackHackServiceTest {
 
         int actual = 1000;
         int expected = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     public void testWhenValueAlreadyReached() {
@@ -40,6 +40,43 @@ public class CashbackHackServiceTest {
 
         int actual = 0;
         int expected = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testJunit5UpperBorderValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount =  999;
+
+        int actual = 1;
+        int expected = service.remain(amount);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testJunit5LowerBorderValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount =  1;
+
+        int actual = 999;
+        int expected = service.remain(amount);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testJunit5ZeroValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount =  0;
+
+        int actual = 1000;
+        int expected = service.remain(amount);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testJunit5WhenValueAlreadyReached() {
+        CashbackHackService service = new CashbackHackService();
+        int amount =  1000;
+
+        int actual = 0;
+        int expected = service.remain(amount);
+        Assertions.assertEquals(expected,actual);
+
     }
 }
